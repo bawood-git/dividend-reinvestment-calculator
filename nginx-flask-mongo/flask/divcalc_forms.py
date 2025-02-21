d7a8209af2279a589484ae2ad7b603d626a13268
Benjamin Wood <bawood@live.com>
2025-02-21 10:43:31 -0500

    Changed logic to start with capital instead of shares owned, search results no longer in report layout, Report Summary simplified (again)

--- nginx-flask-mongo/flask/divcalc_forms.py
+++ nginx-flask-mongo/flask/divcalc_forms.py
@@ -2,13 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, DecimalField, TextAreaField, SubmitField, SelectField, HiddenField
 from wtforms.validators import DataRequired, Length, NumberRange    
 
-
-
 class DivCalcForm(FlaskForm):
     csrf_token      = HiddenField()
     stock_symbol    = HiddenField()
     share_price     = HiddenField()
-    shares_owned    = IntegerField(default=100, validators=[DataRequired(), NumberRange(min=1)])
+    initial_capital = DecimalField(default=1000.00, validators=[DataRequired(), NumberRange(min=0.01)])
+    shares_owned    = DecimalField(default=0.00, validators=[DataRequired(), NumberRange(min=0.00)])
     distribution    = DecimalField(default=0.00)
     term            = IntegerField(default=12)
     frequency       = SelectField(choices=[
@@ -24,7 +23,8 @@ class DivCalcForm(FlaskForm):
     run             = SubmitField()
 
 class SettingsForm(FlaskForm):
-    shares_owned    = IntegerField(default=0)
+    initial_capital = DecimalField(default=1000.00, validators=[DataRequired(), NumberRange(min=0.01)])
+    shares_owned    = DecimalField(default=0.00)
     term            = IntegerField(default=12)
     frequency       = SelectField(choices=[
                                       ('Monthly', 'Monthly'),
--- nginx-flask-mongo/flask/divcalc_server.py
+++ nginx-flask-mongo/flask/divcalc_server.py
@@ -88,16 +88,9 @@ def report():
         settings_form.purchase_mode.data = request.form.get('purchase_mode')          
 
         tab_div_header  = render_template("tab_div_header.jinja")
-        tab_div_profile = render_template('tab_div_profile.jinja', 
-                                          model = data_model)
-        tab_div_finance = render_template('tab_div_financial.jinja', 
-                                          model = data_model)
-        tab_div_history = render_template('tab_div_history.jinja',   
-                                          model = data_model)
         tab_div_calc    = render_template('tab_div_calc.jinja', 
-                                          form  = settings_form, 
-                                          data  = data_model)
-        
+                                          form = settings_form, 
+                                          data = data_model)
         
         ###########################################################
         #
@@ -118,36 +111,39 @@ def report():
         summary = ''
 
         # Dict parameters -> vars for cleaner math
-        term          = int(settings_form.term.data)
-        contribution  = float(settings_form.contribution.data)     
-        shares_owned  = float(settings_form.shares_owned.data)   
-        share_price   = float(settings_form.share_price.data)
-        volatility    = float(settings_form.volatility.data)   
-        distribution  = float(settings_form.distribution.data)
-        purchase_mode = settings_form.purchase_mode.data
-        frequency     = settings_form.frequency.data
-
-        # Init summary totals
-        totals = {
-            "months":0,
-            "shares_owned":0,
-            "last_dividend":0.00,
-            "contributions":0.00,
-            "asset_growth":0.00,
-            "starting_assets":shares_owned * share_price,
-            "ending_assets":0.00,
-            "starting_distribution":shares_owned * distribution,
-            "ending_distribution":0.00,
-            "total_reinvested":0.00,
-            "distribution_growth":0.00
-        }
-        
-        # Initial balance
-        balance = float('0.00')
+        term            = int(settings_form.term.data)
+        initial_capital = float(settings_form.initial_capital.data)   
+        shares_owned    = float(settings_form.shares_owned.data)
+        contribution    = float(settings_form.contribution.data)     
+        share_price     = float(settings_form.share_price.data)
+        volatility      = float(settings_form.volatility.data)   
+        distribution    = float(settings_form.distribution.data)
+        purchase_mode   = settings_form.purchase_mode.data
+        frequency       = settings_form.frequency.data
 
         ###########################################################
         # MAIN LOOP
         ###########################################################
+        
+        # Create position based on initial capital
+       
+        balance = 0.00
+        # Fractional - Use all capital (initial and recurring contributions) to purchase shares
+        if purchase_mode == 'Fractional':
+            shares_owned += initial_capital / share_price
+        # Modulus - Maintain a balance carryover, which would be < price of 1 share
+        else:
+            shares_purchased = initial_capital // share_price
+            shares_owned += shares_purchased
+            balance = initial_capital - (shares_purchased * share_price)
+        
+        # Init summary totals
+        totals = {
+            "contributions":0.00,
+            "starting_assets":shares_owned * share_price,
+            "starting_distribution":shares_owned * distribution,
+            "total_reinvested":0.00,
+        }
         
         # Calculate each period
         for p in range(1, 1 + term):
@@ -208,45 +204,44 @@ def report():
                income_sequence = 2
             case 'Annual':
                income_sequence = 1
-                 
+        total_years = term / income_sequence 
                         
         totals['periods'] = p
         totals['years_vested'] = term / income_sequence
-        
-        
+       
+       # Assets/value
+        totals['cash'] = balance
         totals['shares_owned'] = shares_owned
-        
-        totals['ending_distribution'] = distribution * shares_owned
-        totals['distribution_growth_tot'] =  100 * (totals['ending_distribution'] / totals['starting_distribution'])
-        totals['distribution_growth_avg'] = totals['distribution_growth_tot'] / term
-
         totals['ending_assets'] = shares_owned * share_price
         totals['asset_growth_tot'] = 100 * (totals['ending_assets'] / totals['starting_assets'])
         totals['asset_growth_avg'] = totals['asset_growth_tot'] / term
-
         
+        # Dividends/income
+        totals['ending_distribution'] = distribution * shares_owned
+        totals['distribution_growth_tot'] = totals['ending_distribution'] - totals['starting_distribution']
+        totals['distribution_growth_pct'] = 100 * (totals['distribution_growth_tot'] / totals['starting_distribution'])
+        totals['distribution_growth_avg'] = totals['distribution_growth_tot'] / term
+        totals['reinvestment_gain'] = 100 * ((totals['distribution_growth_pct'] / 100) - (data_model.financials['annual_yield'] * total_years))
+        totals['yield_years'] = 100 * (data_model.financials['annual_yield'] * total_years)
         totals['income_annual'] =  (distribution * shares_owned) * income_sequence
-        totals['income_monthly'] = totals['income_annual'] / 12
-
         
-        
-        summary = render_template('tab_div_summary.jinja', totals=totals)
+        summary = render_template('tab_div_summary.jinja', totals=totals, model=data_model)
 
 # Roll up chart data
 
         # History
-        div_history_labels = []
+        div_history_dates = []
         div_history_amount = []
-            
+        
         for d in data_model.dividend_history:
-            div_history_labels.append(d['payment_date'])
+            div_history_dates.append(d['payment_date'])
             div_history_amount.append(d['amount'])
                 
         #labels = [ d for d in data_model.dividend_history[0]]
         #y1     = [ d for d in data_model.dividend_history[1]]
         tab_div_chart_hist = render_template('tab_div_chart_hist.jinja',
-                                labels = div_history_labels, 
-                                amount = div_history_amount)        
+                                div_dates   = div_history_dates, 
+                                div_amounts = div_history_amount)        
   
         # Simulation
         i = len(report)
@@ -263,20 +258,13 @@ def report():
 # Roll up report
         return render_template('report.jinja', 
                                 header     = tab_div_header,        # Widget
-                                profile    = tab_div_profile,       # Widget
-                                metrics    = tab_div_finance,       # Widget
-                                history    = tab_div_history,       # Widget
                                 config     = tab_div_calc,          # Widget
                                 rows       = rows,                  # Data? FIXME
                                 summary    = summary,               # Data? FIXME
-                                chart_hist = tab_div_chart_hist,    # Widget    
                                 chart_sim  = tab_div_chart_sim,     # Widget
-                                ticker=data_model.profile['stock_symbol']
+                                model      = data_model             # Data
                                 ), 200, {'ContentType':'text/html; charset=utf-8'}
         
-    #except Exception as e:
-    #    return e, 200, {'ContentType':'text/html; charset=utf-8'}
-
 @app.route('/search', methods=['POST'])
 def search():
 
@@ -292,11 +280,11 @@ def search():
         data_model = DataModel(symbol)
         
         # No records returned, error out
-        if data_model == None:
+        if not hasattr(data_model, 'profile'):
             msg= '''
                 <p> No records matched your search term: {symbol}</p>
                 <p> Only individual stocks are available for research at this time. ETFs, mutual funds, and the like will not appear.</p>
-                <p> If you are unsure, try searching for the company stock with your favorite search engine. Some popular picks include Apple (AAPL), Microsoft (MSFT), and Alphabet (GOOG).</p>            
+                <p> If you are unsure, try searching for the company stock with your favorite search engine. Some dividend stocks to try: KO, CVX, UPS, ARR, SHIP</p>            
             '''.format(symbol=symbol)
             return render_template('error.jinja', msg=msg, header=tab_div_header)
 
@@ -305,25 +293,44 @@ def search():
             
             # Init config defaults based on search result and cookie settings
             settings_form = DivCalcForm()
+            
             # Hidden fields. Better way?
             settings_form.stock_symbol.data  = symbol
             settings_form.share_price.data   = data_model.financials['stock_price']
-            # Mix of company stock data and prefernces
-            settings_form.shares_owned.data  = int(request.cookies.get('shares_owned'))
+            
+            # Prefernces
+            if request.cookies.get('shares_owned') != None:
+                settings_form.shares_owned.data  = Decimal(request.cookies.get('shares_owned'))
+            else:
+                settings_form.shares_owned.data  = Decimal(0.00)
+            if request.cookies.get('term') != None:
+                settings_form.term.data = int(request.cookies.get('term'))
+            else:
+                settings_form.term.data = int(120)
+            if request.cookies.get('frequency') != None:
+                settings_form.frequency.data = request.cookies.get('frequency')
+            else:
+                settings_form.frequency.data = 'Monthly'
+            if request.cookies.get('contribution') != None:
+                settings_form.contribution.data = Decimal(request.cookies.get('contribution'))
+            else:
+                settings_form.contribution.data = Decimal(0.00)
+            if request.cookies.get('purchase_mode') != None:
+                settings_form.purchase_mode.data = request.cookies.get('purchase_mode')
+            else:
+                settings_form.purchase_mode.data = 'Modulus'
+            
+            # Company stock data 
             settings_form.distribution.data  = Decimal(data_model.financials['dividend'])
-            settings_form.term.data          = int(request.cookies.get('term'))
-            settings_form.frequency.data     = request.cookies.get('frequency')
-            settings_form.contribution.data  = Decimal(request.cookies.get('contribution'))
             settings_form.volatility.data    = Decimal(data_model.financials['beta'])
-            settings_form.purchase_mode.data = request.cookies.get('purchase_mode')            
 
             # Chart Stuff
-            labels = []
-            amount = []
+            div_dates = []
+            div_amounts = []
             
             for d in data_model.dividend_history:
-                labels.append(d['payment_date'])
-                amount.append(d['amount'])
+                div_dates.append(d['payment_date'])
+                div_amounts.append(d['amount'])
                 
             #labels = [ d for d in data_model.dividend_history[0]]
             #y1     = [ d for d in data_model.dividend_history[1]]
@@ -334,8 +341,8 @@ def search():
             tab_div_finance = render_template('tab_div_financial.jinja', model=data_model)
             tab_div_history = render_template('tab_div_history.jinja',   model=data_model)
             tab_div_chart   = render_template('tab_div_chart_hist.jinja', 
-                                               labels = labels,
-                                               amount = amount
+                                               div_dates   = div_dates,
+                                               div_amounts = div_amounts
                                                )
             tab_div_calc    = render_template('tab_div_calc.jinja',      form=settings_form, data=data_model)
 
@@ -356,11 +363,30 @@ def settings():
 
         # Load form, cookies -> fields
         settings_form = SettingsForm()
-        settings_form.shares_owned.data  = Decimal(request.cookies.get('shares_owned'))
-        settings_form.term.data          = int(request.cookies.get('term'))
-        settings_form.frequency.data     = request.cookies.get('frequency')
-        settings_form.contribution.data  = Decimal(request.cookies.get('contribution'))
-        settings_form.purchase_mode.data = request.cookies.get('purchase_mode')
+        if request.cookies.get('initial_capital') != None:
+            settings_form.shares_owned.data = Decimal(request.cookies.get('initial_capital'))
+        else:
+            settings_form.shares_owned.data  = Decimal(1000.00)
+        if request.cookies.get('shares_owned') != None:
+            settings_form.shares_owned.data = Decimal(request.cookies.get('shares_owned'))
+        else:
+            settings_form.shares_owned.data  = Decimal(100.00)
+        if request.cookies.get('term') != None:
+            settings_form.term.data = int(request.cookies.get('term'))
+        else:
+            settings_form.term.data = int(12)
+        if request.cookies.get('frequency') != None:
+            settings_form.frequency.data = request.cookies.get('frequency')
+        else:
+            settings_form.frequency.data = 'Monthly'
+        if request.cookies.get('contribution') != None:
+            settings_form.contribution.data = Decimal(request.cookies.get('contribution'))
+        else:
+            settings_form.contribution.data = Decimal(0.00)
+        if request.cookies.get('purchase_mode') != None:
+            settings_form.purchase_mode.data = request.cookies.get('purchase_mode')
+        else:
+            settings_form.purchase_mode.data = 'Modulus'
     
         # render form 
         tab_div_settings = render_template('tab_div_settings.jinja', settings=settings_form)
@@ -373,6 +399,7 @@ def settings():
        
          # Load form data -> dict
         cookie_settings = {
+            "initial_capital"  : request.form.get('initial_capital'),
             "shares_owned"  : request.form.get('shares_owned'),
             "term"          : request.form.get('term'),
             "frequency"     : request.form.get('frequency'),
@@ -382,17 +409,18 @@ def settings():
         
         # Load form, init fields from request
         settings_form = SettingsForm()
-        settings_form.shares_owned.data  = request.form.get('shares_owned')
-        settings_form.term.data          = request.form.get('term')
-        settings_form.frequency.data     = request.form.get('frequency')
-        settings_form.contribution.data  = request.form.get('contribution')
-        settings_form.purchase_mode.data = request.form.get('purchase_mode')
+        settings_form.initial_capital.data = request.form.get('initial_capital')
+        settings_form.shares_owned.data    = request.form.get('shares_owned')
+        settings_form.term.data            = request.form.get('term')
+        settings_form.frequency.data       = request.form.get('frequency')
+        settings_form.contribution.data    = request.form.get('contribution')
+        settings_form.purchase_mode.data   = request.form.get('purchase_mode')
         
         # Render html components
         tab_div_settings = render_template('tab_div_settings.jinja', settings=settings_form)
         response = make_response(render_template('settings.jinja', 
                                                  header  = tab_div_header, 
-                                                 content = tab_div_settings
+                                                 content = tab_div_settings,
                                                  ), 200, {'ContentType':'text/html; charset=utf-8'})
         # Write dict -> cookies
         for k, v in cookie_settings.items():
